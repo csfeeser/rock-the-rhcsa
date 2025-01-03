b12275258d99a31fa4f48a8e5946677ca4052c27
Chad Feeser <31425191+csfeeser@users.noreply.github.com>
2025-01-03 14:23:50 -0500

    Update network_check.sh

--- check/network_check.sh
+++ check/network_check.sh
@@ -3,9 +3,9 @@
 # Check hostname
 if [[ $(hostnamectl hostname) == "millennium-falcon" ]]; then
     # Check IP address, gateway, and DNS
-    if sudo nmcli connection show dummy0 | grep -q "192.168.50.100" &&
-       sudo nmcli connection show dummy0 | grep -q "192.168.50.1" &&
-       sudo nmcli connection show dummy0 | grep -q "8.8.8.8"; then
+    if sudo nmcli connection show ens4 | grep -q "192.168.50.100" &&
+       sudo nmcli connection show ens4 | grep -q "192.168.50.1" &&
+       sudo nmcli connection show ens4 | grep -q "8.8.8.8"; then
         echo -e "\e[32mSUCCESS!\e[0m"
     else
         echo -e "\e[31mNO PASS- TRY AGAIN!\e[0m"
