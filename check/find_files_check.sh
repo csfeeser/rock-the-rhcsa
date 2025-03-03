81ca5d500997cd65b69dfdeb332e6386c808a702
Chad Feeser <31425191+csfeeser@users.noreply.github.com>
2025-03-03 11:24:44 -0500

    Update find_files_check.sh

--- check/find_files_check.sh
+++ check/find_files_check.sh
@@ -3,20 +3,12 @@
 # Directory to check
 DIR="/root/findresults"
 
-# Check if directory exists
-if [ -d "$DIR" ]; then
-    # Count the number of files in the directory
-    FILE_COUNT=$(sudo find "$DIR" -maxdepth 1 -type f | wc -l)
+FILE_COUNT=$(sudo find "$DIR" -mindepth 1 -maxdepth 1 -type f | wc -l)
 
-    # Check if the count is between 4 and 5
-    if [ "$FILE_COUNT" -ge 4 ] && [ "$FILE_COUNT" -le 5 ]; then
-        echo -e "\e[32mSUCCESS!\e[0m"
-        exit 0
-    else
-        echo -e "\e[31mNO PASS- TRY AGAIN!\e[0m"
-        exit 1
-    fi
+if [ "$FILE_COUNT" -ge 8 ] && [ "$FILE_COUNT" -le 9 ]; then
+    echo -e "\e[32mSUCCESS!\e[0m"
+    exit 0
 else
     echo -e "\e[31mNO PASS- TRY AGAIN!\e[0m"
-    exit 2
+    exit 1
 fi
